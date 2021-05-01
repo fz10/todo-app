9952a4e8c2e9d3683f2534784e6c8aee1feeee69
Felipe Zapata <f.zapata@utp.edu.co>
2021-05-01 17:18:33 -0500

    Completed

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,41 @@
+import { TodoService } from './../../services/todo.service';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Todo } from './../../models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo = new Todo();
+
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+  }
+
+  // set Dynamic Classes
+  setClasses(): object {
+    const classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    };
+    return classes;
+  }
+
+  onToggle(todo: Todo): void {
+    // Toggle in UI
+    todo.completed = !todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
+  }
+
+  onDelete(todo: Todo): void {
+    this.deleteTodo.emit(todo);
+  }
+
+}
